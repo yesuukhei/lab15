30f2a1f856bd99e1a960ebe2edf94b1c96efed91
Yesuukehi <yesuu1028@gmail.com>
2025-05-07 17:38:24 +0800

    error handling added

--- src/main/java/main/java/Division.java
+++ src/main/java/main/java/Division.java
@@ -2,10 +2,14 @@ package main.java;
 
 public class Division {
     public double divide(double a, double b) {
-    if (b == 0) {
-    throw new IllegalArgumentException("Tegeer huvaaj bolohgue");
+        if (Double.isNaN(a) || Double.isNaN(b)) {
+            throw new IllegalArgumentException("Too oruulalt buruu baina.");
+        }
+
+        if (b == 0) {
+            throw new IllegalArgumentException("Тэгээр хувааж болохгүй.");
+        }
+
+        return a / b;
     }
-    return a / b;
-    }
-   }
-   
+}
